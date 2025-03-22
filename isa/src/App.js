7833bb9510a739558899347c6d17dfa1e89dfeeb
Alena <alenapumprova@seznam.cz>
2025-03-22 17:56:25 +0100

    Přidání složky isa jako běžné části projektu

---
+++ isa/src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+
+const App = () => {
+  const url = "http://api.open-notify.org/iss-now.json"
+  const [latitude, setLatitude] = useState("") 
+  const [longitude, setLongitude] = useState("")
+  const [urlMap, setUrlMap] = useState("") 
+  const [map, setMap] = useState("") 
+
+  const getCoordinates = async() => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setLatitude (data["iss_position"]["latitude"])
+    setLongitude (data["iss_position"]["longitude"])
+
+    const iss_long = data["iss_position"]["longitude"]
+    const iss_lat = data["iss_position"]["latitude"]
+    setUrlMap(`https://www.google.com/maps?q=${iss_lat},${iss_long}&z=4&output=embed`)
+    
+  }
+
+  useEffect ( () => {
+    getCoordinates()
+  }, [])
+
+  return (
+    <div className="hero">
+      <h1>ISA Position</h1>
+      <div className="coordinates">
+        <div className="lat">
+          <h2>Latitude</h2>
+          <p>{latitude}</p>
+        </div>
+
+        <div className="long">
+          <h2>Longitude</h2>
+          <p>{longitude}</p>
+        </div>
+      </div>
+      <iframe
+          src={urlMap}
+          width="50%"
+          height="450"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="ISS Map"
+        ></iframe>
+    </div>
+
+  )
+}
+
+export default App
